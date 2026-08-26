98d46a6b6265d3c26a1aa9ca101c0954e14aaf9d
krako2 <krakowiakjanusz@yahoo.com>
2026-08-26 04:50:56 +0100

    extract debug function to separate translation unit

--- src/main.c
+++ src/main.c
@@ -7,35 +7,7 @@
 #include "util.h"
 #include "file.h"
 #include "game.h"
-
-void print_debug_info(struct game *game) {
-	printf("\n");
-	printf("Player name: %s\n", game->player.name);
-	printf("Introductory text: %s", game->introductory_text);
-	printf("\n");
-	printf("[ROOMS]\n");
-	printf("\n");
-
-	for (size_t i = 0; i < MAX_ROOMS; ++i) {
-		if (game->rooms[i].room_number == 0) {
-			break;
-		}
-
-		printf("Room number: %zu\n", game->rooms[i].room_number);
-		printf("Room message: %s\n", game->rooms[i].message);
-		printf("North connection: %zu\n", game->rooms[i].connections[NORTH]);
-		printf("East connection: %zu\n", game->rooms[i].connections[EAST]);
-		printf("South connection: %zu\n", game->rooms[i].connections[SOUTH]);
-		printf("West connection: %zu\n", game->rooms[i].connections[WEST]);
-		printf("\n");
-
-		for (size_t j = 0; j < MAX_CHALLENGES_PER_ROOM; j++) {
-			printf("Challenge %zu: %u\n", j + 1, game->rooms[i].challenges[j]);
-		}
-
-		printf("\n");
-	}
-}
+#include "debug.h"
 
 int main(void) {
 	static struct game game = {0};
